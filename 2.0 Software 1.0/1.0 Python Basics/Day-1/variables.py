b0471753eec2083266614fa3703e26ddf5829a4c
sangwajesly <sangwajesly82@gmail.com>
2025-07-23 14:25:21 +0100

    Done leraning variables. Created a simple area & perimeter Rectangle calculatoer and a Celsius to fahrenheit converter

---
+++ 2.0 Software 1.0/1.0 Python Basics/Day-1/variables.py
@@ -0,0 +1,64 @@
+#Variables
+# Variables are used to store data values
+# Variables are memory locations that hold data & can change over time or be updated
+# Variables help in code reusability and organization
+
+# name = "David"
+
+# print("I am Jesly, I love this name ~Jesly, was given to me by my father, Paul Jesly, and I really love the name Jesly.")
+# print("I am " + name + ", I love this name ~" + name + ", was given to me by my father, Paul " + name + ", and I really love the name " + name + ".")
+
+
+
+# whole_number = 42
+# decimal_number = 3.14159
+# complex_number = 2 + 3j
+
+# #text
+# greeting = "Hello, World!"
+# single_char = 'A'
+
+
+# #boolean (True/False)
+# is_funny = True
+# is_rainning = False
+
+# #checking the type of variable
+# print(type(whole_number))  # <class 'int'> 
+# print(type(decimal_number))  # <class 'float'>
+
+
+# first_name = "Jesly"
+# last_name = "Sangwa"
+
+# combined_name = first_name + " " + last_name
+# print("My name is " + combined_name + ".")
+# print(f"My name is {combined_name}.")  # Using f-string for formatting
+
+# A ban name generator
+
+# name, age, favorite_meal, department, best_friend
+
+# name = input("Whats your name: ")
+# age = input("How old are you: ")
+# fovorite_meal = input("Your fovorite meal is: ")
+# department = input("What department arre you: ")
+# best_friend = input(" who is youtr best friend")
+
+# print(f"My name is {name} I am {age}yrs old. I love {fovorite_meal} so much. I'm in the {department} in COLTECH. And my best friend is {best_friend}.S")
+
+# A simple calculator
+
+# length = input("Enter the length of the rectangle: ")
+# width = input("Enter the width of the rectangle: ")
+# area = float(length) * float(width)
+# perimeter = 2 * (float(length) + float(width))
+
+# print(f"The area of the rectangle is {area} square units.")
+# print(f"The perimeter of the rectangle is {perimeter} units.")
+
+
+# Convert Celsius to Fahrenheit
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius}°C is equal to {fahrenheit}°F.")
